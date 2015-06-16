78b7ac6c3e62d6ec827a3a9e4b5ce7fe875dbf67
Josef <93121@fhwn.ac.at>
2015-06-16 13:59:59 +0100

    Josef - Client Agent

---
+++ ClientAgent/Client.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+using System.Net;
+
+namespace ClientAgent_PW_Cerima_Koini_V1
+{
+    class Client
+    {
+        public TcpClient ClientTCP { get; set; }
+
+        private Thread ClientThread;
+
+        public bool SendDataAvailable { get; set; }
+
+        public string Message { get; set; }
+
+        public bool Alive { get; set; }
+
+        public void SendMessageToServer()
+        {
+            this.Message = Console.ReadLine();
+            this.SendDataAvailable = true;
+        }
+
+        public void Worker()
+        {
+            NetworkStream netStream = this.ClientTCP.GetStream();
+            byte[] receiveBuffer = new byte[500];
+            byte[] sendBuffer = new byte[500];
+            while (this.Alive)
+            {
+                if (netStream.DataAvailable)
+                {
+                    int readLength = netStream.Read(receiveBuffer, 0, receiveBuffer.Length);
+                    string message = Encoding.UTF8.GetString(receiveBuffer, 0, readLength);
+                    netStream.Flush();
+                    Console.WriteLine(message);
+                }
+                if (this.SendDataAvailable)
+                {
+                    sendBuffer = Encoding.UTF8.GetBytes(this.Message);
+                    netStream.Write(sendBuffer, 0, sendBuffer.Length);
+                    this.SendDataAvailable = false;
+                }
+                Thread.Sleep(50);
+            }
+        }
+
+        public void Connect(string ip, int port)
+        {
+            this.Alive = true;
+            this.ClientTCP = new TcpClient();
+            this.ClientTCP.Connect(IPAddress.Parse(ip), port);
+            this.ClientThread = new Thread(new ThreadStart(Worker));
+            this.ClientThread.Start();
+        }
+    }
+}
---
+++ ClientAgent/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientAgent_PW_Cerima_Koini_V1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Client c = new Client();
+            c.Connect("127.0.0.1", 1337);
+            while (true)
+            {
+                c.SendMessageToServer();
+            }
+            Console.ReadKey();
+        }
+    }
+}
