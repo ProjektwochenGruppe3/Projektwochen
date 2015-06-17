9aca7364da778fce4b8baec23636be3b3ab4c9d6
Josef <93121@fhwn.ac.at>
2015-06-17 16:33:04 +0100

    Josef - Agent

--- ClientAgent/AtomicJob.cs
+++ ClientAgent/AtomicJob.cs
@@ -12,8 +12,13 @@ namespace ClientAgent
         public AtomicJob(Assembly asbly)
         {
             this.Job_Code = asbly;
+            this.Executable = false;
         }
 
         public Assembly Job_Code { get; set; }
+
+        public IEnumerable<object> Params { get; set; }
+
+        public bool Executable { get; set; }
     }
 }
