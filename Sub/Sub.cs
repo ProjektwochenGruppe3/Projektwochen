752c40118fc43fb3b90c2f6874010cfbf8f00297
Kristof <kristof.schwarz@gmx.at>
2015-06-17 12:04:04 +0200

    Kristof - SubComponent

---
+++ Sub/Sub.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Core.Component;
+
+namespace Sub
+{
+    public class Sub : IComponent
+    {
+
+        public Sub()
+        {
+            this.ComponentGuid = new Guid();
+            this.FriendlyName = "3_Sub";
+            this.InputHints = new string[] { "int", "int" };
+            this.OutputHints = new string[] { "int" };
+        }
+        public Guid ComponentGuid
+        {
+            get;
+            private set;
+        }
+
+        public IEnumerable<object> Evaluate(IEnumerable<object> values)
+        {
+            int a;
+            int b;
+            List<object> intList = new List<object>();
+            intList = values.ToList();
+            a = (int)intList[0];
+            b = (int)intList[1];
+            List<object> resultList = new List<object>();
+            resultList[0] = a - b;
+            return resultList;
+        }
+
+        public string FriendlyName
+        {
+            get;
+            private set;
+        }
+
+        public IEnumerable<string> InputHints
+        {
+            get;
+            private set;
+        }
+
+        public IEnumerable<string> OutputHints
+        {
+            get;
+            private set;
+        }
+    }
+}
