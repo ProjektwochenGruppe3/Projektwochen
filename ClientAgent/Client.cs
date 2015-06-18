1fcb441d5565f23c636d54917abc1ae98d4e14ac
Josef <93121@fhwn.ac.at>
2015-06-18 11:00:15 +0100

    Josef - Agent

--- ClientAgent/Client.cs
+++ ClientAgent/Client.cs
@@ -65,7 +65,7 @@ namespace ClientAgent
             NetworkStream netStream = this.ClientTCP.GetStream();
             while (this.Alive)
             {
-                AgentKeepAliveResponse response = new AgentKeepAliveResponse(this.firstKeepAliveGuid, this.MyGuid, this.MyName, 75);
+                AgentKeepAliveResponse response = new AgentKeepAliveResponse(this.firstKeepAliveGuid, this.MyGuid, this.MyName, CPU_Diagnostic.GetCPUusage());
                 Networking.SendPackage(response, netStream);
                 Thread.Sleep(3000);
             }
