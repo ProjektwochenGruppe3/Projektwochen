667a4061216daaa3627ef5e0625f7d46453292be
Thomas Hatter <thomas.hatter@fhwn.ac.at>
2015-06-18 15:48:41 +0200

    servercomp list

--- ServerAgent/EditorHandler.cs
+++ ServerAgent/EditorHandler.cs
@@ -16,12 +16,8 @@ namespace ServerAgent_PW_Josef_Benda_V1
         public EditorHandler(List<Component> localcomponents, List<Component> remotecomponents)
         {
             this.ConnectedEditors = new List<Editor>();
-            this.ComponentList = localcomponents;
-
-            foreach (var item in remotecomponents)
-            {
-                this.ComponentList.Add(item);
-            }
+            this.ComponentList = new List<Component>();
+            
 
             this.Listener = new TcpListener(IPAddress.Any, 30000);
         }
@@ -33,6 +29,24 @@ namespace ServerAgent_PW_Josef_Benda_V1
         private TcpListener Listener { get; set; }
 
         private List<Component> ComponentList { get; set; }
+
+        private List<Tuple<Guid, string>> AvailableClients { get; set; }
+
+        public void UpdateComponentList(List<Component> localcomponents, List<Component> remotecomponents)
+        {
+            this.ComponentList = localcomponents;
+
+            foreach (var item in remotecomponents)
+            {
+                this.ComponentList.Add(item);
+            }
+        }
+
+        public void UpdateClientList()
+        {
+
+        }
+
 
         private void ListenerWorker()
         {
@@ -56,10 +70,10 @@ namespace ServerAgent_PW_Josef_Benda_V1
         {
             Editor editor = obj as Editor;
             NetworkStream ns = editor.TcpClient.GetStream();
-            
+
             try
             {
-                Networking.SendPackage(this.ComponentList, ns);
+                Networking.SendPackage(new ServerComponentList(this.ComponentList, this.AvailableClients), ns);
             }
             catch
             {
@@ -71,7 +85,12 @@ namespace ServerAgent_PW_Josef_Benda_V1
             {
                 if (ns.DataAvailable)
                 {
-                    //Networking.RecievePackage(ns);
+                    EditorJob job = Networking.RecievePackage(ns) as EditorJob;
+
+                    if (job == null)
+                    {
+
+                    }
 
                     break;
                 }
--- dcs.core/ServerComponentList.cs
+++ dcs.core/ServerComponentList.cs
@@ -9,11 +9,14 @@ namespace dcs.core
 {
     public class ServerComponentList
     {
-        public ServerComponentList(List<Component> components)
+        public ServerComponentList(List<Component> components, List<Tuple<Guid, string>> clients)
         {
             this.ComponentList = components;
+            this.AvailableClients = clients;
         }
 
         public List<Component> ComponentList { get; set; }
+
+        public List<Tuple<Guid, string>> AvailableClients { get; set; }
     }
 }
