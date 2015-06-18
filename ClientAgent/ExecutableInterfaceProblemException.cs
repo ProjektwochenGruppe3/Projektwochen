2da795861811f69c5ec80103041043231053968e
Josef <93121@fhwn.ac.at>
2015-06-18 20:31:00 +0100

    Josef - Last commit

---
+++ ClientAgent/ExecutableInterfaceProblemException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ClientAgent
+{
+    public class ExecutableInterfaceProblemException : ArgumentException
+    {
+        public ExecutableInterfaceProblemException(string variable)
+            : base(variable)
+        {
+        }
+    }
+}
