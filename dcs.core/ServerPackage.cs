cede54361625d1ac888bb32b63312df0b48a9c7c
Thomas Hatter <thomas.hatter@fhwn.ac.at>
2015-06-24 16:00:04 +0200

    networking

---
+++ dcs.core/ServerPackage.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Core.Network;
+
+namespace dcs.core
+{
+    public class ServerPackage
+    {
+        public ServerPackage(MessageCode code, object payload)
+        {
+            this.MessageCode = code;
+            this.Payload = payload;
+        }
+
+        public MessageCode MessageCode { get; set; }
+
+        public string Payload { get; set; }
+    }
+}
